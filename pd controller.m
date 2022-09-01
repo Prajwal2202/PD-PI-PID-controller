10d07cd630addeab49ee84ccd741b1c132dece1b
Prajwal <112462883+Prajwal2202@users.noreply.github.com>
2022-09-01 17:37:31 +0530

    Add files via upload

---
+++ pd controller.m
@@ -0,0 +1,17 @@
+%PD controller
+kd=
+kp=
+n=
+d1=conv([],[])
+d=conv(d1,[])
+g=tf(n,d)
+gc=tf([kp kd],1)
+g0=series(g,gc)
+figure(1)
+bode(g0)
+margin(g0)
+figure(2)
+steo(g0)
+g1=feedback(g0,-1)
+figure(3)
+step(g1)
---
+++ pidcontroller.m
@@ -0,0 +1,18 @@
+%PID controller
+ki=
+kd=
+kp=
+n=
+d1=conv([],[])
+d=conv(d1,[])
+g=tf(n,d)
+gc=tf([ki kp kd],1)
+g0=series(g,gc)
+figure(1)
+bode(g0)
+margin(g0)
+figure(2)
+steo(g0)
+g1=feedback(g0,-1)
+figure(3)
+step(g1)
